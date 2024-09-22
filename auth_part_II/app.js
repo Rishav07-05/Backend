43bcd55e5d1eb535b68107fe72287df5ae01050e
Rishav Kumar <rishavkumar18902@gmail.com>
2024-09-23 01:54:05 +0530

    deep down with authentication and authorization

---
+++ auth_part_II/app.js
@@ -0,0 +1,78 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const path = require("path");
+const userModel = require("./models/user");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
+
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+
+app.post("/create", (req, res) => {
+let { username, email, password, age } = req.body;
+
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let createdUser = await userModel.create({
+        username,
+        email,
+        password : hash,
+        age,
+      });
+        
+        let token = jwt.sign({ email }, "sjjewoiwewli");
+        res.cookie("token" , token)
+        
+        res.send(createdUser);
+        
+
+    });
+  });
+    
+
+});
+
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+
+
+
+app.post('/login', async(req, res) => {
+    let user = await userModel.findOne({ email: req.body.email });
+    if (!user) return res.send("Email or password is incorrect");
+
+    bcrypt.compare(req.body.password, user.password, (err, result) => {    
+        let token = jwt.sign({ email: user.email }, "sjjewoiwewli");
+        res.cookie("token", token);
+        if (result) res.send("Logged in successfully");
+        else res.send("Something went wrong");
+    })
+})
+ 
+
+
+
+app.get('/logout', (req, res) => {
+    res.cookie('token', "");
+    // res.send('Successfully logged out');
+    res.redirect('/');
+})
+
+app.listen(5000);
